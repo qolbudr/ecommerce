bf824bcd7ad9557640efe171ae5b4f766ffd1b4a
qolbudr <qolbu.dzikru@protonmail.com>
2025-11-26 18:17:23 +0700

    feat(vascomm): :zap: add login and register

---
+++ src/app/(user)/register/page.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/shared/components/Button";
+import { Input } from "@/shared/components/Input";
+
+const UserRegister: React.FC = () => {
+    return <>
+        <div className="h-screen items-center justify-center flex ">
+            <div className="w-full lg:w-1/2 font-poppins">
+                <div className="flex items-center justify-center h-full px-6 lg:px-16">
+                    <div className="w-full max-w-md ">
+                        <h2 className="text-2xl mb-2">Selamat Datang User</h2>
+                        <p className="text-xs text-secondary">Silahkan isi form berikut untuk melakukan pendaftaran</p>
+                        <form className="space-y-3 mt-8">
+                            <Input filled={false} label="Email" type="text" placeholder="Contoh: user@gmail.com"/>
+                            <Input filled={false} label="Nomor Telepon" type="text" placeholder="Contoh: 08123456789"/>
+                            <Input filled={false} label="Password" type="password" placeholder="Masukkan password"/>
+                            <Button fullWidth className="mt-8" type="submit">Daftar</Button>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </>
+}
+
+export default UserRegister;
--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -1,19 +1,22 @@
 import { Icon, IconifyIcon } from "@iconify/react";
 import classNames from "classnames";
+import { Loader } from "./Loader";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     variant?: 'primary' | 'secondary' | 'outline';
     icon?: string | IconifyIcon;
     fullWidth?: boolean;
     dense?: boolean;
+    loading?: boolean;
 }
 
-export const Button: React.FC<ButtonProps> = ({ variant = 'primary', dense = true, fullWidth = false, ...props }) => {
+export const Button: React.FC<ButtonProps> = ({ variant = 'primary', dense = true, fullWidth = false, loading = false, ...props }) => {
     const className = classNames({
         'bg-primary text-white': variant === 'primary',
         'border border-primary text-primary': variant === 'outline',
         'w-full': fullWidth,
         'tracking-widest': !dense,
+        '!bg-neutral-200 !text-neutral-600': loading,
     },
         'inline-flex py-2.5 px-4 font-bold text-center cursor-pointer transition-colors duration-200 items-center justify-center',
         props.className,
@@ -24,7 +27,7 @@ export const Button: React.FC<ButtonProps> = ({ variant = 'primary', dense = tru
             {...props}
             className={className}>
             {props.icon && <Icon icon={props.icon} className="mr-2" />}
-            {props.children}
+            {loading ? <Loader /> : props.children}
         </button>
     );
 }
