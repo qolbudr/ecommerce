809742c6505328a785a5bdd26b62de3a3497a9c4
qolbudr <qolbu.dzikru@protonmail.com>
2025-11-27 00:21:39 +0700

    feat(vascomm): :art: update api middleware

--- src/shared/middleware/api.middleware.ts
+++ src/shared/middleware/api.middleware.ts
@@ -7,6 +7,8 @@ const SECRET_KEY = process.env.JWT_SECRET;
 export async function handleApiMiddleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
 
+  if(req.method === "GET" && req.nextUrl.pathname.includes('product')) return NextResponse.next();
+
   if (!token) {
     return new NextResponse(JSON.stringify({ message: "Unauthorized", error: "You should login to access this page" }), {
       status: 401,
