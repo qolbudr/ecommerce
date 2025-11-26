0a42e2745c10be3d546134e9f2af30fb3af2da0e
qolbudr <qolbu.dzikru@protonmail.com>
2025-11-26 16:15:04 +0700

    feat(vascomm): :zap: dashboard

--- src/module/admin/components/Sidebar.tsx
+++ src/module/admin/components/Sidebar.tsx
@@ -3,9 +3,9 @@ import { SidebarItem } from "./SidebarItem"
 export const AdminSidebar: React.FC = () => {
     return <div className="w-[300px] py-4 h-full bg-white">
         <ul>
-            <SidebarItem active={false} title="Manajemen Kategori" icon="mdi:home-outline" />
-            <SidebarItem active={false} title="Manajemen User" icon="mdi:user" />
-            <SidebarItem active={false} title="Manajemen Produk" icon="mdi:box" />
+            <SidebarItem path={"/admin/dashboard"} title="Manajemen Kategori" icon="mdi:home-outline" />
+            <SidebarItem path={"/admin/user"} title="Manajemen User" icon="mdi:user" />
+            <SidebarItem path={"/admin/product"} title="Manajemen Produk" icon="mdi:box" />
         </ul>
     </div>
 }
--- src/module/admin/components/SidebarItem.tsx
+++ src/module/admin/components/SidebarItem.tsx
@@ -1,13 +1,19 @@
+'use client';
+
 import { Icon } from "@iconify/react";
 import classNames from "classnames";
-import React from "react";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
-export const SidebarItem = ({active, title, icon}: {active: boolean, title: string, icon?: string}) => {
+export const SidebarItem = ({ title, icon, path }: { title: string, icon?: string, path: string }) => {
+    const router = useRouter();
+    const pathname = usePathname();
+
     const classname = classNames({
-        'bg-primary text-white': active,
+        'bg-primary text-white': pathname === path,
     });
 
-    return <li className={classname + " hover:bg-primary py-3 px-5 hover:text-white cursor-pointer"}>
+    return <li onClick={() => router.push(path)} className={classname + " hover:bg-primary py-3 px-5 hover:text-white cursor-pointer"}>
         {
             icon && <Icon icon={icon} className="inline-flex mb-1.5 mr-2 size-5" />
         }
